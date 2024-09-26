8260ea079d8a9ca3d1f7857ce74748740baff737
rzrusu <rzrusu03@gmail.com>
2024-09-26 12:23:13 +0300

    feat: work on features section

---
+++ components/Section.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+    leftaligned: boolean, title: string, text: string, image: string
+}>()
+</script>
+
+<template>
+    <div class="grid grid-cols-4 py-32 items-center gap-24">
+        <div :class="{ 'order-last': !leftaligned }" class="space-y-5 w-full col-span-2">
+            <h2 class="font-work font-semibold text-3xl title text-[#1F1F1F]"> 
+                {{ title }}
+            </h2>
+            <p class="text-[#4D4D4D] w-4/5">
+                {{ text }}
+            </p>
+        </div>
+        <img :src="`../public/${image}.webp`" alt="Football" class="w-full bg-gray-300 rounded-lg min-h-96 col-span-2">
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -31,10 +31,35 @@ const items = ref([
         <BlogPost v-for="item in items" :post="item"/>
       </div>
     </div>
-    <div>
+    <div class="space-y-48">
       <div>
         <SectionHeader text="Start from humble beginnings"/>
+        <Section :leftaligned="true" title="Begin Your Journey" text="Start small by playing in local leagues, hone your skills, and build your reputation step by step. Every great career has humble origins." image="test" />
+        <Section :leftaligned="false" title="Overcome Challenges" text="Face challenges both on and off the field. Work hard, rise above setbacks, and showcase your potential to move up the ranks." image="test" />
+        <Section :leftaligned="true" title="Make a Name for Yourself" text="As you prove your talent, scouts take notice. Sign your first professional contract and set the stage for a legendary career." image="test" />
       </div>
+      
+      <div>
+        <SectionHeader text="Conquer the world stage"/>
+        <Section :leftaligned="true" title="Play for Elite Teams" text="Join top clubs around the world and compete in prestigious leagues and tournaments. Represent your country and become a global star." image="test" />
+        <Section :leftaligned="false" title="Make Iconic Performances" text="Deliver match-winning performances, break records, and etch your name in the history books. Let the world witness your greatness." image="test" />
+        <Section :leftaligned="true" title="Expand Your Global Influence" text="Take part in media appearances, commercials, and major events. Grow your fanbase and influence on and off the field." image="test" />
+      </div>
+      
+      <div>
+        <SectionHeader text="Build meaningful relationships"/>
+        <Section :leftaligned="true" title="Form Bonds with Teammates" text="Build strong relationships with teammates, coaches, and fans. Support each other to achieve greatness together." image="test" />
+        <Section :leftaligned="false" title="Engage with Fans" text="Connect with your loyal supporters, give back to the community, and create lasting memories through your interactions." image="test" />
+        <Section :leftaligned="true" title="Build a Legacy" text="Mentor younger players, foster deep connections, and leave behind a lasting impact both on and off the pitch." image="test" />
+      </div>
+      
+      <div>
+        <SectionHeader text="Acquire high value assets"/>
+        <Section :leftaligned="true" title="Accumulate Wealth" text="Earn lucrative contracts, endorsement deals, and investments. Secure your financial future through smart decisions." image="test" />
+        <Section :leftaligned="false" title="Own Luxury Properties" text="Acquire prestigious real estate in prime locations around the world. Live the life of a superstar both at home and abroad." image="test" />
+        <Section :leftaligned="true" title="Build Your Empire" text="Diversify your assets with businesses, brand deals, and other investments to create a lasting empire beyond football." image="test" />
+      </div>
+      
     </div>
   </div>
 </template>
