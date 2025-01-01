a9bcd56a25df07224630957bf84f3af8a1810b74
rzrusu <rzrusu03@gmail.com>
2025-01-01 22:54:48 +0200

    Create Container.vue

---
+++ components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-4 bg-white w-full rounded-xl items-center highlight space-y-2">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+
+<style>
+    .highlight {
+        box-shadow: 0px 4px 0px 0px #E5E5E5;
+    }
+</style>
